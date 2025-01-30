f4c56d9aaa5ec6d2bbc07681500c5795baa43dac
Spappz <34202141+Spappz@users.noreply.github.com>
2025-01-30 16:38:04 +0000

    Defer `macro` payload to own file
    I've gotten confused where it's stored too many times 😩

--- schema/payload.ts
+++ schema/payload.ts
@@ -1,32 +1,13 @@
 import { z } from 'zod';
-import { ID, predicate, rollOption, UUID } from './helpers/atoms';
+import { ID, predicate, rollOption } from './helpers/atoms';
 import { nonEmpty, uniqueItems } from './helpers/refinements';
 import { pluralise } from './helpers/utils';
 import { animationPayload } from './payloads/animation';
 import { crosshairPayload } from './payloads/crosshair';
 import { graphicPayload } from './payloads/graphic';
+import { macroPayload } from './payloads/macro';
 import { soundPayload } from './payloads/sound';
 import { trigger } from './triggers';
-
-/**
- * Zod schema for a `macro`-type payload.
- */
-const macroPayload = z.object({
-	type: z.literal('macro'),
-	document: UUID.describe('The UUID of the macro to be executed.'),
-	everyoneExecutes: z
-		.literal(true)
-		.optional()
-		.describe(
-			'By default, only the triggering user executes the macro. If you\'d like every logged-in user to execute the macro, enable this option.',
-		),
-	options: z
-		.object({})
-		.optional()
-		.describe(
-			'An arbitrary object of options you can pass into the macro as an argument. The following properties are always available unless overwritten:\n- `sources`: an array of token objects that triggered the payload.\n- `targets`: an array of token objects targetted by the triggering `sources`.\n- `templates`: an array of template objects associated with the trigger.\n- `user`: the user ID (string) for the triggering user.\n- `item`: the item object containing the triggered roll option (if any).\n- `currentIndex`: in a set with multiple payloads, this is the number of the currently executing payload (starting from 0)',
-		),
-});
 
 /**
  * Zod schema for the animation-set payload that actually gets executed.
