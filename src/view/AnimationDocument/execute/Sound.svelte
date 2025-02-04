fcc1370764ab6ebfe645db82bfb753855f383f65
mrvauxs <mrvauxs@gmail.com>
2025-02-04 22:07:03 +0100

    Add basic sound support

--- src/view/AnimationDocument/execute/Sound.svelte
+++ src/view/AnimationDocument/execute/Sound.svelte
@@ -3,6 +3,9 @@
 
 	export let data: AnimationSetContentsItem<'sound'>;
 	export let readonly: boolean;
+
+	// @ts-expect-error TODO: Sequencer Types
+	const entries: string[] = window.Sequencer.Database.publicFlattenedSimpleEntries;
 </script>
 {#if !data.execute}
 	{#if !readonly}
@@ -12,12 +15,28 @@
 	<div class='space-y-2'>
 		<label class='grid grid-cols-3 items-center'>
 			<span class='flex items-center' data-tooltip='TODO: Explain'>
-				Text Text
+				Sound
 				<i class='fa fa-info-circle px-2 ml-auto'></i>
 			</span>
 			<div class='flex align-middle items-center col-span-2'>
+				<datalist id='graphic'>
+					{#each entries as name}
+						<option>{name}</option>
+					{/each}
+				</datalist>
 				<input
+					list='graphic'
 					type='text'
+					value={data.execute.sound}
+					on:change={(e) => {
+						if (!data.execute) data.execute = {};
+						if (e.currentTarget.value) {
+							data.execute.sound = [e.currentTarget.value];
+						} else {
+							data.execute.sound = [];
+						};
+					}}
+					{readonly}
 					disabled={readonly}
 				/>
 			</div>
