ba1b73efc1b37c1448aeba080372daea9a6f02f2
mrvauxs <mrvauxs@gmail.com>
2025-02-04 21:29:53 +0100

    Jason! JASON!

--- src/view/_components/JSONEditor.svelte
+++ src/view/_components/JSONEditor.svelte
@@ -1,30 +1,53 @@
 <script lang='ts'>
-	import type { Mode } from 'svelte-jsoneditor';
+	import type { Mode, ValidationError } from 'svelte-jsoneditor';
+	import { dev } from 'src/utils';
+	import { ValidationSeverity } from 'svelte-jsoneditor';
+	import { fromZodIssue } from 'zod-validation-error';
 
 	export let json: object;
 	export let mode: Mode = 'text' as Mode;
+
+	async function validatorFactory(): Promise<(x: unknown) => ValidationError[]> {
+		const module = await import('schema/validation');
+		return (json: unknown): ValidationError[] => {
+			const result = module.validateAnimationData(json);
+			if (result.success) return [];
+			return result.error.issues.map(issue => ({
+				path: issue.path.map(piece => piece.toString()),
+				message: fromZodIssue(issue, { prefix: null, includePath: false }).toString(),
+				severity: ValidationSeverity.error,
+			}));
+		};
+	}
 </script>
 
-{#await import('svelte-jsoneditor')}
-	Waiting for extra JSONEditor code...
-{:then Module}
-	<Module.JSONEditor
-		content={{ json }}
-		readOnly={true}
-		{mode}
-		navigationBar={false}
-		statusBar={false}
-		indentation='	'
-		tabSize={2}
-	/>
-{/await}
-
-<style>
-	:global(button.jse-last) {
-		display: none !important;
-	}
-
-	:global(.jse-button.jse-group-button:not(.jse-last)) {
-		border-right: 1px solid var(--jse-menu-color, var(--jse-text-color-inverse, #fff)) !important;
-	}
-</style>
+<section class='pf2e-g'>
+	{#await import('svelte-jsoneditor')}
+		Waiting for extra JSONEditor code...
+	{:then Module}
+		{#if dev}
+			{#await validatorFactory() then validator}
+				<Module.JSONEditor
+					content={{ json }}
+					readOnly={true}
+					{mode}
+					{validator}
+					navigationBar={false}
+					statusBar={false}
+					indentation='	'
+					tabSize={2}
+				/>
+			{/await}
+		{:else}
+			<Module.JSONEditor
+				content={{ json }}
+				readOnly={true}
+				{mode}
+				navigationBar={false}
+				statusBar={false}
+				indentation='	'
+				tabSize={2}
+			/>
+		{/if}
+	{/await}
+</section>
