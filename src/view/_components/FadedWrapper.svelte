90a5ae9725fdc087b07d747a4b2ae70c4e96d3b0
mrvauxs <mrvauxs@gmail.com>
2025-02-17 17:20:24 +0100

    Fancy up that wrapper

--- src/view/_components/FadedWrapper.svelte
+++ src/view/_components/FadedWrapper.svelte
@@ -1,3 +1,36 @@
-<div class='flex flex-col gap-2 p-1 border border-solid rounded-sm bg-slate-600/15'>
-	<slot></slot>
+<script lang='ts'>
+	import { backIn, backOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+
+	export let classes = '';
+
+	let hidden = true;
+</script>
+
+<div class='flex flex-col gap-2 p-1 border border-solid rounded-sm bg-slate-600/15 {classes}'>
+	{#if $$slots.title}
+		<div class='flex flex-row flex-nowrap gap-2 items-center'>
+			<div class='grow'>
+				<slot name='title'></slot>
+			</div>
+			<button class='w-min' on:click={() => hidden = !hidden}>
+				{#if hidden}
+					<i in:slide={{ duration: 300, easing: backOut }} class='fa fa-chevron-down fa-fw mx-auto'></i>
+				{:else}
+					<i in:slide={{ duration: 300, easing: backOut }} class='fa fa-chevron-up fa-fw mx-auto'></i>
+				{/if}
+			</button>
+		</div>
+
+		{#if !hidden}
+			<div
+				in:slide={{ duration: 300, easing: backOut }}
+				out:slide={{ duration: 300, easing: backIn }}
+			>
+				<slot></slot>
+			</div>
+		{/if}
+	{:else}
+		<slot></slot>
+	{/if}
 </div>
