4ed086ab2ef23f12a896906160282a9e4d3ad72b
Spappz <34202141+Spappz@users.noreply.github.com>
2025-04-20 19:44:11 +0100

    Minorly improve `generic` description

--- schema/payload.ts
+++ schema/payload.ts
@@ -181,7 +181,7 @@ const generic = z
 	])
 	.optional()
 	.describe(
-		'An animation can be marked as \'generic\'. This means that it doesn\'t just describe one unique event, but rather a general occurrence that might occur in many contexts (such as Casting a Spell, or scoring a critical hit on a Strike).\nThere are two parts to generic animations.\n- First, on the receiving end, we have `type: "slot"` generics. This indicates that this animation is a vacant spot for that general occurrence; it does nothing on its own, but indicates a slot (!) that an `add-on` can fill.\n- `add-on` generics are the premade \'templates\' that are actually applied. An `add-on` either fills a `slot` with a matching `label`, or it\'s simply prepended/appended the an animation set whenever it applies if `label` is undefined. You can control the position the `add-on` is placed using `order`.',
+		'An animation can be marked as \'generic\'. This means that it doesn\'t just describe one unique type of event, but rather a general event (e.g. Casting a Spell, scoring a critical hit on a Strike) that might occur in many contexts (e.g. different spells, different weapons).\nThere are two parts to generic animations.\n- First, on the receiving end, we have `type: "slot"` generics. This indicates that this animation is a vacant spot for that general occurrence; it does nothing on its own, but indicates a slot (!) that an `add-on` can fill.\n- `add-on` generics are the premade \'templates\' that are actually applied. An `add-on` either fills a `slot` with a matching `label`, or it\'s simply prepended/appended the an animation set whenever it applies if `label` is undefined. You can control the position the `add-on` is placed using `order`.',
 	);
 
 /**
